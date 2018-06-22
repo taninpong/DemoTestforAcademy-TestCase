92ff29200dd876ea25b6947318d2a802cc251d17
thaninpong <thaninpong@perfenterprise.com>
2018-06-22 15:27:12 +0700

    add project

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testclass = new ImplementClass();
+            var resual = testclass.ImplementTest();
+        }
+
+
+        [Fact]
+        public void xxx()
+        {
+
+        }
+    }
+}
